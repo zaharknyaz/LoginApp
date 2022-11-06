1d63df535f48f7e00f6f9d5e4051161c8c0e822d
Zahar Knyazev <knyazzz1989@mail.ru>
2022-11-06 20:13:53 +0100

    LoginApp ver4

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = loginTF.text
+    }
+    
+    // Метод для скрытия клавиатуры тапом по экрану
+    //override func touchesBegan(_ touches: Set, with event: UIEvent?) {
+    //    super .touchesBegan(touches, with: event)
+    //}
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        //guard let welcomeVC = segue.source as? WelcomeViewController else { return }
+        loginTF.text = ""
+        passwordTF.text = ""
+    }
+    
 }
+
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        userNameLabel.text = userName
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
